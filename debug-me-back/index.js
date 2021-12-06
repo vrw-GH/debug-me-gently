76743e802ed50435d930a0149e986b6414d76ed3
Victor Wright <victor.wright@outlook.de>
2021-12-06 13:30:59 +0100

    backend mvp

--- debug-me-back/index.js
+++ debug-me-back/index.js
@@ -1,24 +1,47 @@
-const express = require('express');
-const upload = require('utils/imageUploader')
+const express = require("express");
+//! const upload = require('utils/imageUploader')
+const upload = require("./utils/imageUploader");
+//! added path
+const path = require("path");
 
-const app = express;
-const port = Math.floor(Math.random() * 65535)
+//! const app = express;
+const app = express();
+//! const port = Math.floor(Math.random() * 65535);
+const port = 5000;
 
-app.use('/*', (req, res) => {
-  return res.send('Connected successfully to the server')
-})
+//! app.use(express.static(path.join(__dirname, "/public/uploads")));
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.use(express.static(path.join(__dirname, '/public/uploads')));
+app.post("/upload-profile-pic", upload.single("profile_pic"), (req, res) => {
+  //! const { file, fileValidationError } = req;
+  const { file, fileValidationError } = req;
+  if (!file) {
+    res.status(400).send("Please upload a file"); // 400 Bad Request
+    // ! to abandon function without crashing
+    return;
+  }
+  if (fileValidationError) {
+    res.status(400).send(fileValidationError);
+  }
+  const newFile = `./public/${file.filename}`;
+  // res.render({ pathToImage: `/uploads/${file.originalname}` });
+  res.send(`file uploaded to ${newFile}`);
+});
 
-app.post('/upload-profile-pic', upload.single('profile_pic'), (req, res) => {
-    const {file, fileValidationError} = res
-    if (!file) {
-      res.status(400).send('Please upload a file') // 400 Bad Request
-    }
-    if (fileValidationError) {
-      res.status(400).send(fileValidationError);
-    }
-    res.render({pathToImage: `/uploads/${file.originalname}`});
-  })
+//! get single pic
+app.get("/public/:profile_pic", (req, res) => {
+  const getFile = req.params.profile_pic;
+  // res.attachment(`./public/${getFile}`).send();
+  res.sendFile(`./public/${getFile}`, { root: __dirname });
+});
 
-app.listen(port, () => console.log(`Listening on port 3000...`));
+//! catchall?
+// app.use("/*", (req, res) => {
+app.get("/*", (req, res) => {
+  return res.send(
+    "Connected successfully to the server.\nValid endpoints are:- \nPOST (/upload-profile-pic form-data:<profile_pic> with valid image) \nGET (/<image_name>.<ext>)"
+  );
+});
+
+//! app.listen(port, () => console.log(`Listening on port 3000...`));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
--- debug-me-back/utils/imageUploader.js
+++ debug-me-back/utils/imageUploader.js
@@ -1,27 +1,35 @@
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => { 
-    cb(null, './public/uploads')
-  },
+  //! destination: (req, file, cb) => {
+  //   cb(null, "./public/uploads");
+  // },
+  destination: "./public",
   filename: (req, file, cb) => {
-    cb(file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    //! cb(file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+//! const limits = { fileSize: 1000 };
+const limits = { fileSize: 100000 };
+
+//! const fileFilter = (req, file, cb) {
+const fileFilter = (req, file, cb) => {
+  // Accept image file types only
+  console.log(file);
+  //! if (!file.fieldname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+    req.fileValidationError = "Only image files are allowed!";
+    return cb(new Error("Only image files are allowed!"), false);
   }
-})
-
-const limits = { fileSize: 1000 }
-
-const fileFilter = (req, file, cb) {
-    // Accept image file types only
-    console.log(file)
-    if (!file.fieldname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
-        req.fileValidationError = 'Only image files are allowed!';
-        return cb(new Error('Only image files are allowed!'), false);
-    }
-    cb(null, true);
+  cb(null, true);
 };
 
-const upload = multer({ storage, fileFilter, limits })
+const upload = multer({ storage, fileFilter, limits });
 
-module.exports = upload
+module.exports = upload;
